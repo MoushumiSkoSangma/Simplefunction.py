98a5e40f8da48fc6a46c722176cdb83d2e8e543c
Moushumi Sko Sangma <Moushumisangma515@gmail.com>
2025-11-13 20:43:06 +0530

    Add files via upload

---
+++ #simple function.py
@@ -0,0 +1,14 @@
+#simple function 
+def mysum(a,b): #defining a function
+    c=a+b                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              
+    return c
+
+print("before function call")
+
+x=300
+y=400
+result = mysum(x,y)
+print ( result )
+print("after function call")
+
+
