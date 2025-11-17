21b10fd01f67800af563a48f0e963b1f2821d159
Moushumi Sko Sangma <Moushumisangma515@gmail.com>
2025-11-17 22:36:34 +0530

    basic calculation function

---
+++ BASIC calculation function.py
@@ -0,0 +1,11 @@
+def add ( a,d):
+    """ add a and b numbers"""
+    return a + d 
+
+def multiply (a, d):
+    """ multiply two numbers"""
+    return a * d
+
+#usage
+print(add(4,10))
+print(multiply(14,92))
