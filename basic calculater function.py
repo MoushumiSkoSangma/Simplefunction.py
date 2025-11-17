98e9fccf9546757dd1d55739e91cfa23fdeec24a
Moushumi Sko Sangma <Moushumisangma515@gmail.com>
2025-11-17 22:25:25 +0530

    Add files via upload

---
+++ basic calculater function.py
@@ -0,0 +1,11 @@
+def add( a,b):
+    """adds two numbers"""
+    return a + b
+
+def substract( a , b ):
+    """subtracts b from a"""
+    return a * b
+
+#usage 
+print(add(7 ,8))
+print(substract(10, 20))
